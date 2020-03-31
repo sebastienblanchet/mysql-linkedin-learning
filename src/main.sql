8358b8aa1fd56c9de3d83d2e17c171d3ab862ed8
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-31 11:20:51 -0400

    Chapter 5: math

--- src/main.sql
+++ src/main.sql
@@ -1,80 +1,61 @@
--- 01 string comparisons
+-- 01 value functions
+-- continued with some basic excel functionss
+SELECT ABS(-47);
+SELECT ABS(-47.73);
 
-USE world;
--- underscore, single char any
--- percent is any number of any char
-SELECT Name FROM country WHERE Name LIKE '_a%' ORDER BY Name;
--- same as c
-SELECT Name FROM country WHERE STRCMP(Name, 'France') <= 0 ORDER BY Name;
+SELECT CEILING(12.2);
+SELECT CEIL(12.2);
 
--- 02 regular expressions
+SELECT ROUND(17.5);
+SELECT ROUND(17.4);
 
-USE world;
-SELECT Name FROM country WHERE Name RLIKE 'y$' ORDER BY Name;
-SELECT Name FROM country WHERE Name RLIKE '[xz][ai]' ORDER BY Name;
+-- number of decimals, essentially returns
+SELECT TRUNCATE(42.973, 1);
+SELECT TRUNCATE(42.973, 2);
+-- <0 ===> LEFT of .
+SELECT TRUNCATE(99942.973, -2);
 
--- 03 string concatenation
--- this is OR
-SELECT 'This ' || 'and ' || 'that';
-SELECT CONCAT('one', 'two');
-SELECT CONCAT('string', 42);
-SELECT CONCAT(42, 42);
+-- 02 math funcitons
 
--- 04 numeric conversions
+SELECT PI();
+-- FULL PRECISION
+SELECT PI() + 0.000000000000000;
 
--- basic
-SELECT 32742;
-SELECT HEX(32742);
-SELECT OCT(32742);
-SELECT BIN(32742);
+-- SAME
+SELECT POWER(8, 2);
+SELECT POW(8, 2);
 
--- from base to base
-SELECT CONV('32742',10,16);
-SELECT CONV('7FE6',16,10);
-SELECT CONV('28K6',24,10);
+-- FRATIONAL POWERSs
+SELECT SQRT(64);
+SELECT POWER(4096, 1/4);
 
--- 05 trimming and padding
+-- PSEUDO random
+SELECT RAND();
+-- SEED
+SELECT RAND(42);
 
-USE scratch;
--- similar to EXCEL function
-SELECT * FROM customer WHERE name LIKE '  Bill Smith  ';
-SELECT * FROM customer WHERE name LIKE TRIM('  Bill Smith  ');
-SELECT CONCAT(':', RTRIM('  Bill Smith  '), ':');
-SELECT CONCAT(':', LTRIM('  Bill Smith  '), ':');
-SELECT CONCAT(':', TRIM('x' FROM 'xxxBill Smithxxx'), ':');
+-- 03 trigonometry
+-- ARGS in rad
+SELECT SIN(2);
+SELECT ASIN(.2);
+SELECT COS(PI());
+SELECT ACOS(.5);
+SELECT TAN(PI());
+SELECT ATAN(2);
+SELECT COT(12);
 
--- left or right padding of char
-SELECT LPAD('Price', 20, '.');
-SELECT LPAD('Price', 20, '. ');
-SELECT RPAD('Price', 20, '. ');
+-- 04 logarithms
 
--- 06 case conversion
+SELECT LN(2);
+SELECT LOG(2);
+-- provide base
+SELECT LOG(10, 100);
+SELECT LOG(2, 65535);
+SELECT LOG2(65535);
+SELECT LOG10(100);
+SELECT EXP(1);
 
-USE scratch;
--- excel continued
-SELECT UPPER(name) FROM customer;
-SELECT LOWER(name) FROM customer;
-SELECT CONCAT(UPPER(SUBSTRING(name, 1, 1)),LOWER(SUBSTRING(name, 2))) FROM customer;
+-- 05 degrees and radians
 
--- 07 substring
-
-SELECT SUBSTRING('this is a string', 6);
-SELECT SUBSTR('this is a string', 6);
-SELECT SUBSTR('this is a string', 6, 4);
-SELECT SUBSTR('this is a string', -6);
-SELECT SUBSTR('this is a string', -6, 4);
-
-SELECT SUBSTRING_INDEX('this is a string', ' ', 1);
-SELECT SUBSTRING_INDEX('this is a string', ' ', 2);
-SELECT SUBSTRING_INDEX('this is a string', ' ', -2);
-
--- 08 soundex
--- this is hilarious
-SELECT SOUNDEX('bill'), SOUNDEX('bell');
-SELECT SOUNDEX('acton'), SOUNDEX('action');
-SELECT SOUNDEX('acting'), SOUNDEX('action');
-
-SELECT 'bill' SOUNDS LIKE 'boil', 'bill' SOUNDS LIKE 'phil';
-
-
-
+SELECT DEGREES(PI());
+SELECT RADIANS(180);
