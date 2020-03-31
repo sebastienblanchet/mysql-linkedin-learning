2eb8820f3f26e9a9296f98db25c3a447b1eed585
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-31 11:17:22 -0400

    Chapter 4: string

--- src/main.sql
+++ src/main.sql
@@ -1,117 +1,80 @@
--- 01 comparison operators
-
--- comparison ops (==)
-SELECT 0 = 0;
-SELECT 0 = 1;
--- STRING IS CONVERTED TO NUMBER
-SELECT 0 = '0';
-SELECT '0.1' > 0;
-SELECT '0.1' < 0;
-SELECT 0.1 < 0;
-SELECT 9 < 7;
-SELECT 9 > 7;
-SELECT 9 = 7;
-SELECT 9 != 7;
-SELECT 9 <= 7;
-SELECT 9 >= 7;
-SELECT (9 > 7) AND (12 < 27);
-SELECT (9 > 7) OR (12 < 27);
-SELECT (9 > 7) IS TRUE;
-SELECT (9 < 7) IS NOT TRUE; 
--- 1
-
--- 02 logical operators
--- basic logical
-SELECT 1 AND 1;
-SELECT 1 OR 1;
-SELECT 1 OR 0;
-SELECT 1 XOR 0;
-SELECT 1 XOR 1;
-
-SELECT 1 IS TRUE;
-SELECT 1 IS NOT TRUE;
-
-SELECT 1 IS NULL;
-SELECT 1 IS NOT NULL;
-SELECT 0 IS NULL;
-SELECT 0 IS NULL;
-SELECT NULL IS NULL;
-SELECT '' IS NULL;
-
--- TESTING FOR IN A SET
-SELECT 7 IN (1, 5, 9);
-SELECT 9 IN (1, 5, 9);
+-- 01 string comparisons
 
 USE world;
-SELECT Name AS 'Country', ROUND(Population / 1000000) AS 'PopMM'
-  FROM Country
-  -- BASIC OVERVIEW IN ACTION
-  WHERE Population > 50000000 AND Continent IN ('Asia', 'Europe')
-  -- FINAL ORDER
-  ORDER BY Population DESC;
+-- underscore, single char any
+-- percent is any number of any char
+SELECT Name FROM country WHERE Name LIKE '_a%' ORDER BY Name;
+-- same as c
+SELECT Name FROM country WHERE STRCMP(Name, 'France') <= 0 ORDER BY Name;
 
-SELECT t.title AS 'Track', t.track_number AS 'Track No', a.title AS 'Album',
-    a.artist AS 'Artist', t.duration AS 'Seconds'
-  FROM Album AS a
-  JOIN Track AS t ON t.album_id = a.id
-  -- alright
-  WHERE t.duration > 120 AND t.track_number > 3
-  ORDER BY t.duration DESC;
+-- 02 regular expressions
 
--- 03 assignment operators
+USE world;
+SELECT Name FROM country WHERE Name RLIKE 'y$' ORDER BY Name;
+SELECT Name FROM country WHERE Name RLIKE '[xz][ai]' ORDER BY Name;
 
-SELECT 5 + 3;
-SELECT 5 - 3;
-SELECT 5 * 3;
-SELECT 5 / 3;
-SELECT 5 DIV 3;
-SELECT 5 MOD 3;
-SELECT 5 / 0;
--- NULL
+-- 03 string concatenation
+-- this is OR
+SELECT 'This ' || 'and ' || 'that';
+SELECT CONCAT('one', 'two');
+SELECT CONCAT('string', 42);
+SELECT CONCAT(42, 42);
 
--- 04 operator precedence
+-- 04 numeric conversions
 
--- similar to C/C++ other langs
-SELECT 5 + 6 * 7 - 8;
-SELECT (5 + 6) * (7 - 8);
-SELECT 5 + (6 * 7) - 8;
+-- basic
+SELECT 32742;
+SELECT HEX(32742);
+SELECT OCT(32742);
+SELECT BIN(32742);
 
--- 05 The CASE statement
+-- from base to base
+SELECT CONV('32742',10,16);
+SELECT CONV('7FE6',16,10);
+SELECT CONV('28K6',24,10);
 
-USE scratch;
-
-DROP TABLE IF EXISTS booltest;
-CREATE TABLE booltest (a INTEGER, b INTEGER);
-INSERT INTO booltest VALUES (1, 0);
-SELECT * FROM booltest;
-
--- SWITCH CASE
-SELECT
-  CASE WHEN a < 5 THEN 'true' ELSE 'false' END AS boolA,
-  CASE WHEN b > 0 THEN 'true' ELSE 'false' END AS boolB
-  FROM booltest
-;
-
-SELECT
-  CASE a WHEN 1 THEN 'true' ELSE 'false' END AS boolA,
-  CASE b WHEN 1 THEN 'true' ELSE 'false' END AS boolB
-  FROM booltest
-;
-
-DROP TABLE IF EXISTS booltest;
-
--- 06 the IF statement
+-- 05 trimming and padding
 
 USE scratch;
+-- similar to EXCEL function
+SELECT * FROM customer WHERE name LIKE '  Bill Smith  ';
+SELECT * FROM customer WHERE name LIKE TRIM('  Bill Smith  ');
+SELECT CONCAT(':', RTRIM('  Bill Smith  '), ':');
+SELECT CONCAT(':', LTRIM('  Bill Smith  '), ':');
+SELECT CONCAT(':', TRIM('x' FROM 'xxxBill Smithxxx'), ':');
 
-DROP TABLE IF EXISTS booltest;
-CREATE TABLE booltest (a INTEGER, b INTEGER);
-INSERT INTO booltest VALUES (1, 0);
-SELECT * FROM booltest;
+-- left or right padding of char
+SELECT LPAD('Price', 20, '.');
+SELECT LPAD('Price', 20, '. ');
+SELECT RPAD('Price', 20, '. ');
 
--- condition, if true, if false ==> this is excel
-SELECT IF(a < 5, 'TRUE', 'FALSE') FROM booltest;
-SELECT IF(b > 0, 'TRUE', 'FALSE') FROM booltest;
+-- 06 case conversion
 
-DROP TABLE IF EXISTS booltest;
+USE scratch;
+-- excel continued
+SELECT UPPER(name) FROM customer;
+SELECT LOWER(name) FROM customer;
+SELECT CONCAT(UPPER(SUBSTRING(name, 1, 1)),LOWER(SUBSTRING(name, 2))) FROM customer;
+
+-- 07 substring
+
+SELECT SUBSTRING('this is a string', 6);
+SELECT SUBSTR('this is a string', 6);
+SELECT SUBSTR('this is a string', 6, 4);
+SELECT SUBSTR('this is a string', -6);
+SELECT SUBSTR('this is a string', -6, 4);
+
+SELECT SUBSTRING_INDEX('this is a string', ' ', 1);
+SELECT SUBSTRING_INDEX('this is a string', ' ', 2);
+SELECT SUBSTRING_INDEX('this is a string', ' ', -2);
+
+-- 08 soundex
+-- this is hilarious
+SELECT SOUNDEX('bill'), SOUNDEX('bell');
+SELECT SOUNDEX('acton'), SOUNDEX('action');
+SELECT SOUNDEX('acting'), SOUNDEX('action');
+
+SELECT 'bill' SOUNDS LIKE 'boil', 'bill' SOUNDS LIKE 'phil';
+
+
 
